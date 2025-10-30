e1328dc6fe75a893fa0c1a6b9d77e0791efe182d
karida-julia <karidajulia1@gmail.com>
2025-10-30 11:35:47 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,27 @@ function App() {
 
   return (
     <>
+      <nav className={styles.menu}>
+        <a href="#s1">Home</a>
+        <a href="#s2">Cards</a>
+        <a href="#s3">Video</a>
+        <a href="#s4">Contato</a>
+      </nav>
+      <main>
+        <section>
+          <h2>sessao 1</h2>
+        </section>
+        <section>
+          <h2>sessao 2</h2>
+        </section>
+        <section>
+          <h2>sessao 3</h2>
+          </section>
+          <section>
+            <h2>sessao 4</h2>
+          </section>
+        
+      </main>
       <h1 className={styles.titulo}>Karida</h1>
       
     </>
