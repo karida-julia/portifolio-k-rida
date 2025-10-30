07761126f8b4d56d31422b5a0a72b82085ea3479
karida-julia <karidajulia1@gmail.com>
2025-10-30 10:04:37 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import styles from'./App.module.css'
+
+function App() {
+
+//javascript
+
+  return (
+    <>
+      <h1 className={styles.titulo}>Karida</h1>
+      
+    </>
+    
+  )
+}
+
+export default App
