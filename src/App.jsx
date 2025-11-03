01b444cbfd795ddcb9913cbf5b8fff9c9890b9ce
karida-julia <karidajulia1@gmail.com>
2025-11-03 11:32:29 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,45 @@
 
 import styles from'./App.module.css'
-
+import fotoPerfil from '../public/foto.jpg'
 function App() {
 
 //javascript
 
-  return (
+  return(
     <>
       <nav className={styles.menu}>
-        <a href="#s1">Home</a>
-        <a href="#s2">Cards</a>
-        <a href="#s3">Video</a>
-        <a href="#s4">Contato</a>
+      <a href="#s1">Home</a>
+      <a href="#s2">Cards</a>
+      <a href="#s3">Video</a>
+      <a href="#s4">Contato</a>
       </nav>
       <main>
-        <section>
-          <h2>sessao 1</h2>
+        <section  className={styles.s1}
+        id='s1'>
+          <div className={styles.left}>
+            <img className={styles.imgPerfil} src= {fotoPerfil} alt="Foto perfil" />
+          </div>
+          <div className={styles.right}>
+            <h2 className={styles.title}> Kárida Júlia</h2>
+            <p className={styles.paragraph}> Estudante, 17 anos, terminando o ensino médio no terceiro ano, cursando curso tecnico de TEI.</p>
+          </div>
         </section>
-        <section>
-          <h2>sessao 2</h2>
-        </section>
-        <section>
-          <h2>sessao 3</h2>
-          </section>
-          <section>
-            <h2>sessao 4</h2>
-          </section>
         
-      </main>
-      <h1 className={styles.titulo}>Karida</h1>
-      
-    </>
-    
-  )
-}
+        <section id='s2'>
+          <h2>sessão 2</h2>
+        </section>
 
-export default App
+        <section id='s3'>
+          <h2>sessão 3</h2>
+        </section>
+
+        <section id='s4'>
+          <h2>sessão 4</h2>
+        </section>
+
+      </main>
+      </>
+    )
+  }
+
+  export default App
